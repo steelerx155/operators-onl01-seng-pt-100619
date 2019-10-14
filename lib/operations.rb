59eaf737167a0e0a87797668b47f887e0e46e4ea
Randy Steele <steelerx155@gmail.com>
2019-10-14 23:48:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def unsafe?(speed)
   if speed > 60 
     true
@@ -10,4 +11,25 @@ end
 
 def not_safe?(speed)
   speed < 40 || speed > 60 ? true : false
+=======
+def unsafe?(speed) 
+ if speed > 60 
+   return true
+ else speed < 40 > 60
+   return true
+ 
+end
+
+
+
+def not_safe?(speed)
+ speed < 60 ? true : false
+ speed > 40 ? true : false
+ speed > 60 ? false : true
+ speed < 40 ? false : true
+ end
+
+	
+
+>>>>>>> faaae479fb62b7335c4071ff729122c218292b9b
 end
